e2304be5ea7db088f9d76222ae21e85654c89c85
chelinthalingamt <chelinthalingamt@gmail.com>
2025-03-06 20:29:22 +0530

    Updated code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gdsc_frontend/',
+  base: './',  // Ensures correct asset paths for GitHub Pages
   build: {
     outDir: 'dist'
   }
