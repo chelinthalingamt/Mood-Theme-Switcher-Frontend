bead11a6f9ed21bcdd61de4bc6637732ab1fd567
chelinthalingamt <chelinthalingamt@gmail.com>
2025-03-06 19:58:49 +0530

    Updated

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MoodThemeSwitcher from "./MoodThemeSwitcher";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MoodThemeSwitcher />
+  </React.StrictMode>
+);
