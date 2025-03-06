b0a1cda104117ed5a96078e5aff0483df6914382
chelinthalingamt <chelinthalingamt@gmail.com>
2025-03-06 19:27:20 +0530

    Fixed vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gdsc_frontend/',s
+  base: '/gdsc_frontend/',
   build: {
     outDir: 'dist'
   }
